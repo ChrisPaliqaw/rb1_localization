d35c5566497c77cfabac9f5736b4622c7e1c6b1c
Chris Paliqaw <christophomos@gmail.com>
2022-02-24 13:23:58 +0000

    Service server returns true

--- scripts/record_poi_server.py
+++ scripts/record_poi_server.py
@@ -1,51 +1,29 @@
 #!/usr/bin/env python
 
 import rospy
-from rb1_localization.serv import POI, POIRequest, POIResponse
+from rb1_localization.srv import POI, POIRequest, POIResponse
 
 class RecordPOIServer():
-    SERVICE_NAME = "/record_poi_server"
+    SERVICE_NAME = "record_poi_server"
+
+    def __server_request_callback(self, request: POIRequest):
+        response = POIResponse()
+        response.success = True
+        return response
 
     def __init__(self):
-        self._service = rospy.Service(RecordPOIServer.SERVICE_NAME, POIRequest , my_callback)
+        self._service = rospy.Service(RecordPOIServer.SERVICE_NAME, POI , self.__server_request_callback)
         rospy.loginfo(f"Service {RecordPOIServer.SERVICE_NAME} Ready")
-        
-        self._cmd = Twist()
+        self._pois = []
         self._ctrl_c = False
-        self._rate = rospy.Rate(10) # 10hz
         rospy.on_shutdown(self.shutdownhook)
-        
-    def publish_once_in_cmd_vel(self):
-        """
-        This is because publishing in topics sometimes fails the first time you publish.
-        In continuous publishing systems, this is no big deal, but in systems that publish only
-        once, it IS very important.
-        """
-        while not self.ctrl_c:
-            connections = self.bb8_vel_publisher.get_num_connections()
-            if connections > 0:
-                self.bb8_vel_publisher.publish(self.cmd)
-                rospy.loginfo("Cmd Published")
-                break
-            else:
-                self.rate.sleep()
         
     def shutdownhook(self):
         # works better than the rospy.is_shutdown()
         self.ctrl_c = True
 
-    def move_bb8(self, linear_speed=0.2, angular_speed=0.2):
-        
-        self.cmd.linear.x = linear_speed
-        self.cmd.angular.z = angular_speed
-        
-        rospy.loginfo("Moving BB8!")
-        self.publish_once_in_cmd_vel()
             
 if __name__ == '__main__':
-    rospy.init_node('move_bb8_test', anonymous=True)
-    movebb8_object = MoveBB8()
-    try:
-        movebb8_object.move_bb8()
-    except rospy.ROSInterruptException:
-        pass
+    rospy.init_node(RecordPOIServer.SERVICE_NAME, anonymous=True)
+    recordPOIServer = RecordPOIServer()
+    rospy.spin()

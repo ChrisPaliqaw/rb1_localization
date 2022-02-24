214da19b334392e5bdaa299bba86ccb914496bab
Chris Paliqaw <christophomos@gmail.com>
2022-02-24 15:17:49 +0000

    Complete 2.2 Complete service server that saves POI's from /amcl_pose

--- scripts/record_poi_server.py
+++ scripts/record_poi_server.py
@@ -31,24 +31,62 @@ geometry_msgs/PoseWithCovariance pose
 
 import rospy
 from rb1_localization.srv import POI, POIRequest, POIResponse
-from geometry_msgs.msg import PoseWithCovarianceStamped
+from geometry_msgs.msg import PoseWithCovarianceStamped, Pose
+
+def poseTupleTostring(pose_tuple):
+    title = pose_tuple[0]
+    pose = pose_tuple[1]
+    return f"""
+{title}:
+  position:
+    x: {pose.position.x}
+    y: {pose.position.y}
+    z: {pose.position.z}
+  orientation:
+    x: {pose.orientation.x}
+    y: {pose.orientation.y}
+    z: {pose.orientation.z}
+    w: {pose.orientation.w}
+           """
 
 class RecordPOIServer():
-    SERVICE_NAME = "record_poi_server"
+    SERVICE_NAME = "save_poi"
     WAIT_HZ = 1
+    END_MESSAGE = "end"
+    OUT_FILE_NAME = "poi.yaml"
 
-    def __server_request_callback(self, request: POIRequest):
+    def __writeFile(self):
+        with open(RecordPOIServer.OUT_FILE_NAME, 'w') as f:
+            for pose_tuple in self._pois:
+                f.write(poseTupleTostring(pose_tuple))
+
+    def __serverRequestCallback(self, request: POIRequest):
+        success = True
+        if request.label == RecordPOIServer.END_MESSAGE:
+            if len(self._pois) > 0:
+                self.__writeFile()
+                rospy.loginfo(f"Wrote {RecordPOIServer.OUT_FILE_NAME}")
+                self._pois.clear()
+            else:
+                success = False
+        else:
+            while self._last_amcl_pose is None:
+                rospy.loginfo("Waiting for /amcl_pose data...")
+                rospy.sleep(RecordPOIServer.WAIT_HZ)
+            self._pois.append((request.label, self._last_amcl_pose))
+            rospy.loginfo(f"POI list length = {len(self._pois)}")
+            rospy.loginfo(f"{poseTupleTostring(self._pois[-1])}")
         response = POIResponse()
-        response.success = True
+        response.success = success
         return response
 
-    def __amcl_pose_callback(self, poseWithCovarianceStamped: PoseWithCovarianceStamped):
+    def __amclPoseCallback(self, poseWithCovarianceStamped: PoseWithCovarianceStamped):
         self._last_amcl_pose = poseWithCovarianceStamped.pose.pose
-        rospy.loginfo(self._last_amcl_pose)
+        # rospy.loginfo(self._last_amcl_pose)
 
     def __init__(self):
-        self._service = rospy.Service(RecordPOIServer.SERVICE_NAME, POI , self.__server_request_callback)
-        self._amcl_pose_sub = rospy.Subscriber("/amcl_pose", PoseWithCovarianceStamped, self.__amcl_pose_callback)
+        self._service = rospy.Service(RecordPOIServer.SERVICE_NAME, POI , self.__serverRequestCallback)
+        self._amcl_pose_sub = rospy.Subscriber("/amcl_pose", PoseWithCovarianceStamped, self.__amclPoseCallback)
         rospy.loginfo(f"Service {RecordPOIServer.SERVICE_NAME} Ready")
         self._pois = []
         self._last_amcl_pose = None
@@ -61,6 +99,6 @@ class RecordPOIServer():
 
             
 if __name__ == '__main__':
-    rospy.init_node(RecordPOIServer.SERVICE_NAME, anonymous=True)
+    rospy.init_node("record_poi_server", anonymous=True)
     recordPOIServer = RecordPOIServer()
     rospy.spin()
